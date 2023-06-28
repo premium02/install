cab72583aeae5fab8412c47ed679fdea80faa2bb
premium02 <137971329+premium02@users.noreply.github.com>
2023-06-28 10:32:49 +0700

    Update menu-trgo.sh

--- menu/menu-trgo.sh
+++ menu/menu-trgo.sh
@@ -35,7 +35,7 @@ export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/artanodrop/prokontrol/main"
+export Server_URL="raw.githubusercontent.com/premium02/install/main"
 export Server_Port="443"
 export Server_IP="underfined"
 export Script_Mode="Stable"
